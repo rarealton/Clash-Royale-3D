855cce21b4d7908f0e2b7773b8b06ed47b58b752
Joseph Shumway <thegreenjag1@gmail.com>
2017-04-03 21:50:32 -0500

    Updated Sparky materials in ue4 and added marmoset toolbag project

--- Start Git.bat
+++ Start Git.bat
@@ -1,12 +1,12 @@
 @echo off
-git pull
+git pull origin master
 git status
 set /p id="Commit Message: "
 git add .
 set /p confirm="Commit these changes? Y/N: "
 if "%confirm%" =="y" (
 	git commit -m "%id%"
-	git push
+	git pa
 )
 
 pause
