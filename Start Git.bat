2011bb5c7bc625f3151feb7eecee120f9b9131b4
Joseph Shumway <thegreenjag1@gmail.com>
2017-04-03 21:24:34 -0500

    Added sparky textures and fixed agulon and such

--- Start Git.bat
+++ Start Git.bat
@@ -1,2 +1,12 @@
-cmd
+@echo off
 git pull
+git status
+set /p id="Commit Message: "
+git add .
+set /p confirm="Commit these changes? Y/N: "
+if "%confirm%" =="y" (
+	git commit -m "%id%"
+	git push
+)
+
+pause
