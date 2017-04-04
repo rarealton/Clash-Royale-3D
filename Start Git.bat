7424b4766da738afd4df8919b96f6579feb64b72
Joseph Shumway <thegreenjag1@gmail.com>
2017-04-04 17:40:49 -0500

    Fixed an error in commit bat file

--- Start Git.bat
+++ Start Git.bat
@@ -1,12 +1,32 @@
 @echo off
+git pull surreal master
 git pull origin master
+
+cls
 git status
+
+:choice
+@echo.
+@echo 1. Push
+@echo 2. Commit
+set /p pushOrCommit="Choice: "
+
+if "%pushOrCommit%" == "1" (
+	git push
+	pause
+	exit
+)
+
+if not "%pushOrCommit%" == "2" (
+@echo Invalid choice
+goto choice
+)
+
 set /p id="Commit Message: "
 git add .
 set /p confirm="Commit these changes? Y/N: "
-if "%confirm%" =="y" (
+if "%confirm%" == "y" (
 	git commit -m "%id%"
 	git pa
 )
-
 pause
